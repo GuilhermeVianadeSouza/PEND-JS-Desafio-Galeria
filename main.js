1766f11f588e25a26fa8156c915ef36959ff90b8
GUILHERME VIANA DE SOUZA <25132756@dsn127m1246730.cfp127aluno.local>
2025-08-11 10:42:48 -0300

    Partes realizadas com o professor, desafio iniciado de colocar o nome utilizando o javaScript

---
+++ main.js
@@ -0,0 +1,67 @@
+'use strict'
+
+const imagens = [
+    {
+        nome: 'Peixe acara-oscar',
+        url: './img/acara-acu.jpg',
+    },
+    {
+        nome: 'peixe acara-banderensse',
+        url: './img/acara-bandeira.webp'
+    },
+    {
+        nome: 'peixe acara-camaleao',
+        url: './img/acara-camaleao.jpg'
+    },
+    {
+        nome: 'peixe acara-congo',
+        url: './img/acara-congo.webp'
+    },
+    {
+        nome: 'peixe acara-disco',
+        url: './img/acara-disco.jpg'
+    },
+    {
+        nome: 'peixe acara-festivo',
+        url: './img/acara-festivo.webp'
+    },
+    {
+        nome: 'peixe acara-joia',
+        url: './img/acara-joia.png'
+    },
+    {
+        nome: 'peixe acara-pixuna',
+        url: './img/acara-pixuna.jpg'
+    },
+    {
+        nome: 'peixe acara-severum',
+        url: './img/acara-severum.webp'
+    }
+]
+
+function criarImagem(srcImagem){
+    const galeria = document.getElementById('galeria')
+    const imagem = document.createElement('img')
+    imagem.src = srcImagem.url
+    //adiciona um filho para a div principal, sem mexer no html
+    galeria.appendChild(imagem)
+}
+
+function carregarImagens (){
+
+
+    // let contador = 0
+    // const limite = imagens.length
+    // while (contador <= limite - 1){
+    // //criacao de uma tag/ elemento, sem alterar o hmtl, só altero como o chrome ver minha página
+    
+    // contador++
+    // }
+
+    //Percorre array, elemento por elemento.
+    imagens.forEach (criarImagem)
+
+
+}
+
+carregarImagens()
