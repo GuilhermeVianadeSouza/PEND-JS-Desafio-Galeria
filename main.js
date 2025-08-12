dcfdf32c722529a8e2afadbb61cce51798b4cbe9
GUILHERME VIANA DE SOUZA <25132756@CFP127ALUNO.LOCAL>
2025-08-12 08:18:12 -0300

    Adaptando a formatação para JS, ainda há erros a serem ajustado.

--- main.js
+++ main.js
@@ -41,26 +41,35 @@ const imagens = [
 
 function criarImagem(srcImagem){
     const galeria = document.getElementById('galeria')
+    //Criei um container/div para conter os elementos de forma melhor
+    const container = document.createElement('div')
+
     const imagem = document.createElement('img')
     imagem.src = srcImagem.url
+    //Aqui para atribuir o nome
+    imagem.alt = srcImagem.nome
+
+    const legenda = document.createElement('h2')
+    legenda.textContent = srcImagem.nome
     //adiciona um filho para a div principal, sem mexer no html
     galeria.appendChild(imagem)
+    galeria.appendChild(legenda)
+
+    galeria.appendChild(container)
 }
 
 function carregarImagens (){
 
-
-    // let contador = 0
-    // const limite = imagens.length
-    // while (contador <= limite - 1){
-    // //criacao de uma tag/ elemento, sem alterar o hmtl, só altero como o chrome ver minha página
+    let contador = 0
+    const limite = imagens.length
+    while (contador <= limite - 1){
+    //criacao de uma tag/ elemento, sem alterar o hmtl, só altero como o chrome ver minha página
     
-    // contador++
-    // }
+    contador++
+    }
 
     //Percorre array, elemento por elemento.
     imagens.forEach (criarImagem)
-
 
 }
 
